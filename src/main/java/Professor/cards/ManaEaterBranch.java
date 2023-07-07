dd0619ce7ed57e4ba9f1569dc8a081382ae95112
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-07 19:22:27 -0300

    Add Mana-Eater Branch

---
+++ src/main/java/Professor/cards/ManaEaterBranch.java
@@ -0,0 +1,57 @@
+package Professor.cards;
+
+import Professor.actions.EasyXCostAction;
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.util.CardArtRoller;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Professor.MainModfile.makeID;
+
+public class ManaEaterBranch extends AbstractEasyCard {
+    public final static String ID = makeID(ManaEaterBranch.class.getSimpleName());
+
+    public ManaEaterBranch() {
+        super(ID, -1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+        baseDamage = damage = 11;
+        exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new EasyXCostAction(this, (base, params) -> {
+            int effect = base;
+            for (int i : params) {
+                effect += i;
+            }
+            for (int i = 0 ; i < effect ; i++) {
+                addToTop(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.FIRE));
+            }
+            return true;
+        }));
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(3);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, Color.GOLD, WHITE, Color.GOLD, BLACK, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return ManaEaterBranch.class.getSimpleName();
+    }
+}
