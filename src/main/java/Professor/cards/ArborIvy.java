9becb08f3692f3428efcb452d4f332d0a1b0a9d3
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-07 14:56:22 -0300

    Add Arbor Ivy

---
+++ src/main/java/Professor/cards/ArborIvy.java
@@ -0,0 +1,47 @@
+package Professor.cards;
+
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.util.CardArtRoller;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Professor.MainModfile.makeID;
+
+public class ArborIvy extends AbstractEasyCard {
+    public final static String ID = makeID(ArborIvy.class.getSimpleName());
+
+    public ArborIvy() {
+        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        baseBlock = block = 8;
+        baseMagicNumber = magicNumber = 1;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        addToBot(new DrawCardAction(magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        //upgradeBlock(3);
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, GREEN, WHITE, GREEN, WHITE, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return ArborIvy.class.getSimpleName();
+    }
+}
