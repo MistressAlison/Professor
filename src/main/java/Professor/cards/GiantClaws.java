b9be5f82e7de50009b5ac818d91d0683521a111b
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-07 14:56:03 -0300

    Add Giant Claws

---
+++ src/main/java/Professor/cards/GiantClaws.java
@@ -0,0 +1,54 @@
+package Professor.cards;
+
+import CardAugments.actions.ScaleAllByPredAction;
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.util.CardArtRoller;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.ClawEffect;
+
+import static Professor.MainModfile.makeID;
+
+public class GiantClaws extends AbstractEasyCard {
+    public final static String ID = makeID(GiantClaws.class.getSimpleName());
+
+    public GiantClaws() {
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 8;
+        baseMagicNumber = magicNumber = 4;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (m != null) {
+            addToBot(new VFXAction(new ClawEffect(m.hb.cX, m.hb.cY, Color.CYAN, Color.WHITE), 0.1F));
+        }
+        dmg(m, AbstractGameAction.AttackEffect.NONE);
+        addToBot(new ScaleAllByPredAction(this, magicNumber, ScaleAllByPredAction.StatBoost.DAMAGE, c -> c instanceof GiantClaws));
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, Color.BROWN, WHITE, Color.BROWN, BLACK, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return GiantClaws.class.getSimpleName();
+    }
+}
