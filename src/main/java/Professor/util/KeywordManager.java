95799a6041985019ef6ac46565fc9f2fd1dbc6a5
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-08 19:26:20 -0300

    Add Gather mechanic

--- src/main/java/Professor/util/KeywordManager.java
+++ src/main/java/Professor/util/KeywordManager.java
@@ -17,6 +17,7 @@ public class KeywordManager {
     public static String NEUTRALIZER;
     public static String BRACED;
     public static String EXPOSED;
+    public static String GATHER;
 
     public static String BOMB;
     public static String EXPLOSIVE_UNI;
