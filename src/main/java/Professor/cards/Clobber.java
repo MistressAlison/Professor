fe397642a3669ede5f9555b48bcba4c314deb883
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-08 12:50:08 -0300

    Clobber rework

--- src/main/java/Professor/cards/Clobber.java
+++ src/main/java/Professor/cards/Clobber.java
@@ -1,6 +1,7 @@
 package Professor.cards;
 
 import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.powers.ExposedPower;
 import Professor.util.CardArtRoller;
 import Professor.util.Wiz;
 import com.badlogic.gdx.graphics.Color;
@@ -8,7 +9,6 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static Professor.MainModfile.makeID;
 
@@ -16,20 +16,20 @@ public class Clobber extends AbstractEasyCard {
     public final static String ID = makeID(Clobber.class.getSimpleName());
 
     public Clobber() {
-        super(ID, 1, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
-        baseDamage = damage = 4;
-        baseMagicNumber = magicNumber = 1;
+        super(ID, 2, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
+        baseDamage = damage = 10;
+        baseMagicNumber = magicNumber = 4;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
-        Wiz.applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
+        Wiz.applyToEnemy(m, new ExposedPower(m, magicNumber));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(1);
+        upgradeDamage(3);
         upgradeMagicNumber(1);
     }
 
