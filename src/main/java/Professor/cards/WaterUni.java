1296065d97581a03e97d59d15eddd6740578d676
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-10 23:53:15 -0300

    Rework Water Uni

--- src/main/java/Professor/cards/WaterUni.java
+++ src/main/java/Professor/cards/WaterUni.java
@@ -1,37 +1,46 @@
 package Professor.cards;
 
 import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.cards.interfaces.InHandCard;
 import Professor.patches.CustomTags;
 import Professor.util.CardArtRoller;
 import Professor.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.PoisonPower;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static Professor.MainModfile.makeID;
 
-public class WaterUni extends AbstractEasyCard {
+public class WaterUni extends AbstractEasyCard implements InHandCard {
     public final static String ID = makeID(WaterUni.class.getSimpleName());
 
     public WaterUni() {
         super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY);
-        baseMagicNumber = magicNumber = 6;
-        baseSecondMagic = secondMagic = 1;
+        baseBlock = block = 6;
+        baseMagicNumber = magicNumber = 2;
         tags.add(CustomTags.PROF_UNI);
+        selfRetain = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.applyToEnemy(m, new PoisonPower(m, p, magicNumber));
-        Wiz.applyToEnemy(m, new WeakPower(m, secondMagic, false));
+        blck();
+    }
+
+    @Override
+    public void onDamaged(DamageInfo info) {
+        if (info.owner != null && info.owner != Wiz.adp()) {
+            addToTop(new DamageAction(info.owner, new DamageInfo(Wiz.adp(), magicNumber, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL, true));
+        }
     }
 
     @Override
     public void upp() {
-        upgradeMagicNumber(2);
-        upgradeSecondMagic(1);
+        upgradeBlock(2);
+        upgradeMagicNumber(1);
     }
 
     @Override
