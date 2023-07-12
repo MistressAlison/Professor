a2eba1be05325809d6e6d542ef2559b35e931692
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-12 02:04:34 -0300

    Add Shining Sand

---
+++ src/main/java/Professor/cards/ShiningSand.java
@@ -0,0 +1,55 @@
+package Professor.cards;
+
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.util.CardArtRoller;
+import Professor.util.Wiz;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EnergizedBluePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import static Professor.MainModfile.makeID;
+
+public class ShiningSand extends AbstractEasyCard {
+    public final static String ID = makeID(ShiningSand.class.getSimpleName());
+
+    public ShiningSand() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
+        isEthereal = true;
+        baseMagicNumber = magicNumber = 2;
+        baseSecondMagic = secondMagic = 1;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.applyToSelf(new EnergizedBluePower(p, magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        flash();
+        Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new WeakPower(mon, secondMagic, false)));
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, mix(Color.WHITE, Color.GOLD), WHITE, mix(Color.WHITE, Color.GOLD), WHITE, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return VoidCard.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return ShiningSand.class.getSimpleName();
+    }
+}
