57d2ae95151242f5e06de7f2c387de1272c048b6
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-12 23:34:21 -0300

    Amber Oil given new placeholder effect

--- src/main/java/Professor/powers/AmberOilPower.java
+++ src/main/java/Professor/powers/AmberOilPower.java
@@ -1,22 +1,18 @@
 package Professor.powers;
 
 import Professor.MainModfile;
-import Professor.actions.BurnDamageAction;
-import Professor.util.Wiz;
-import com.badlogic.gdx.graphics.Color;
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.BetterOnApplyPowerPower;
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.HealthBarRenderPower;
+import Professor.patches.ArchetypeHelper;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.IntangiblePlayerPower;
-import com.megacrit.cardcrawl.powers.IntangiblePower;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
-public class AmberOilPower extends AbstractPower implements BetterOnApplyPowerPower {
+public class AmberOilPower extends AbstractPower {
 
     public static final String POWER_ID = MainModfile.makeID(AmberOilPower.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
@@ -39,20 +35,10 @@ public class AmberOilPower extends AbstractPower implements BetterOnApplyPowerPo
     }
 
     @Override
-    public boolean betterOnApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
-        if (power instanceof BurnPower && source == owner) {
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (ArchetypeHelper.isFire(card) || ArchetypeHelper.isBolt(card)) {
             flash();
-            power.amount += amount;
+            addToBot(new DamageRandomEnemyAction(new DamageInfo(owner, amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.FIRE));
         }
-        return true;
-    }
-
-    @Override
-    public int betterOnApplyPowerStacks(AbstractPower power, AbstractCreature target, AbstractCreature source, int stackAmount) {
-        if (power instanceof BurnPower && source == owner) {
-            flash();
-            stackAmount += amount;
-        }
-        return stackAmount;
     }
 }
