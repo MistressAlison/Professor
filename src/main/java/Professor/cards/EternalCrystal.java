911336057b8f36a0d9196832610a2f96467f581b
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-12 23:34:56 -0300

    Eternal Crystal given new placeholder effect

--- src/main/java/Professor/cards/EternalCrystal.java
+++ src/main/java/Professor/cards/EternalCrystal.java
@@ -4,12 +4,10 @@ import Professor.cards.abstracts.AbstractEasyCard;
 import Professor.patches.CustomTags;
 import Professor.util.CardArtRoller;
 import Professor.util.Wiz;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.cards.tempCards.Omega;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.powers.MetallicizePower;
 
 import static Professor.MainModfile.makeID;
 
@@ -17,23 +15,19 @@ public class EternalCrystal extends AbstractEasyCard {
     public final static String ID = makeID(EternalCrystal.class.getSimpleName());
 
     public EternalCrystal() {
-        super(ID, 2, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = damage = 15;
-        baseMagicNumber = magicNumber = 2;
+        super(ID, 2, CardType.POWER, CardRarity.RARE, CardTarget.SELF);
+        baseMagicNumber = magicNumber = 5;
         tags.add(CustomTags.PROF_CATALYST);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
-        Wiz.applyToEnemy(m, new WeakPower(m, magicNumber, false));
-        Wiz.applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
+        Wiz.applyToSelf(new MetallicizePower(p, magicNumber));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(3);
-        upgradeMagicNumber(1);
+        upgradeMagicNumber(2);
     }
 
     @Override
@@ -43,7 +37,7 @@ public class EternalCrystal extends AbstractEasyCard {
 
     @Override
     public String cardArtCopy() {
-        return Miracle.ID;
+        return Omega.ID;
     }
 
     @Override
