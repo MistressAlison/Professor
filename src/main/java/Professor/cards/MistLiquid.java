1e5b9a080f635508327d0aa3563b4847222029a9
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-07-13 16:23:23 -0300

    Add Mist Liquid. Fix Amber Oil not stating Catalyst

---
+++ src/main/java/Professor/cards/MistLiquid.java
@@ -0,0 +1,47 @@
+package Professor.cards;
+
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.patches.CustomTags;
+import Professor.powers.MistLiquidPower;
+import Professor.util.CardArtRoller;
+import Professor.util.Wiz;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Professor.MainModfile.makeID;
+
+public class MistLiquid extends AbstractEasyCard {
+    public final static String ID = makeID(MistLiquid.class.getSimpleName());
+
+    public MistLiquid() {
+        super(ID, 1, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = 1;
+        tags.add(CustomTags.PROF_REACTANT);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.applyToSelf(new MistLiquidPower(p, magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, mix(WHITE, AZURE), WHITE, mix(WHITE, AZURE), WHITE, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return VoidCard.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return MistLiquid.class.getSimpleName();
+    }
+}
