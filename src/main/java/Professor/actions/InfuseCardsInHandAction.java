2fae988dda0a416f363474ffd63399cd08fef974
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-09 14:56:47 -0300

    Add SFX to Infuse

--- src/main/java/Professor/actions/InfuseCardsInHandAction.java
+++ src/main/java/Professor/actions/InfuseCardsInHandAction.java
@@ -5,6 +5,7 @@ import Professor.util.Wiz;
 import basemod.abstracts.AbstractCardModifier;
 import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
 import java.util.function.Predicate;
@@ -27,7 +28,10 @@ public class InfuseCardsInHandAction extends ModifyCardsInHandAction {
                     CardModifierManager.addModifier(c, mod.makeCopy());
                     //Wiz.att(new ApplyCardModifierAction(c, mod.makeCopy()));
                 }
+                c.superFlash();
             }
+            CardCrawlGame.sound.play("MONSTER_COLLECTOR_SUMMON", 0.2f);
+            CardCrawlGame.sound.play("RELIC_DROP_MAGICAL", 0.2f);
         });
     }
 }
