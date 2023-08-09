8b263098a1d54290999bdbd12d033f07ef3f3305
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-09 14:11:55 -0300

    Rework Uni

--- src/main/java/Professor/cards/Uni.java
+++ src/main/java/Professor/cards/Uni.java
@@ -1,12 +1,13 @@
 package Professor.cards;
 
+import Professor.actions.ThrowObjectAction;
 import Professor.cards.abstracts.AbstractEasyCard;
 import Professor.patches.CustomTags;
 import Professor.powers.ExposedPower;
 import Professor.util.CardArtRoller;
 import Professor.util.Wiz;
 import com.badlogic.gdx.graphics.Color;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -17,21 +18,22 @@ public class Uni extends AbstractEasyCard {
     public final static String ID = makeID(Uni.class.getSimpleName());
 
     public Uni() {
-        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.ENEMY);
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
         baseMagicNumber = magicNumber = 4;
-        secondMagic = baseSecondMagic = 1;
+        baseDamage = damage = 4;
         tags.add(CustomTags.PROF_UNI);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ThrowObjectAction(itemArt(), 1/3f, m.hb, Color.BROWN));
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_DIAGONAL);
         Wiz.applyToEnemy(m, new ExposedPower(m, magicNumber));
-        addToBot(new DrawCardAction(secondMagic));
     }
 
     @Override
     public void upp() {
-        //upgradeDamage(2);
+        upgradeDamage(2);
         upgradeMagicNumber(2);
     }
 
