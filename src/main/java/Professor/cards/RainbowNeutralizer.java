0078a031bb40a0d6eb040df7d8566e44aa9fe1f2
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-10 13:48:04 -0300

    Magma Powder now Rainbow Neutralizer

--- src/main/java/Professor/cards/RainbowNeutralizer.java
+++ src/main/java/Professor/cards/RainbowNeutralizer.java
@@ -5,17 +5,17 @@ import Professor.util.CardArtRoller;
 import Professor.util.Wiz;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.cards.tempCards.Omega;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 import static Professor.MainModfile.makeID;
 
-public class MagmaPowder extends AbstractEasyCard {
-    public final static String ID = makeID(MagmaPowder.class.getSimpleName());
+public class RainbowNeutralizer extends AbstractEasyCard {
+    public final static String ID = makeID(RainbowNeutralizer.class.getSimpleName());
 
-    public MagmaPowder() {
+    public RainbowNeutralizer() {
         super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         selfRetain = true;
         baseDamage = damage = 15;
@@ -40,16 +40,16 @@ public class MagmaPowder extends AbstractEasyCard {
 
     @Override
     public CardArtRoller.ReskinInfo reskinInfo(String ID) {
-        return new CardArtRoller.ReskinInfo(ID, darken(Color.PURPLE), WHITE, mix(RED, ORANGE), WHITE, false);
+        return new CardArtRoller.ReskinInfo(ID, darken(RED), YELLOW, pastel(Color.PINK), pastel(YELLOW), false);
     }
 
     @Override
     public String cardArtCopy() {
-        return VoidCard.ID;
+        return Omega.ID;
     }
 
     @Override
     public String itemArt() {
-        return MagmaPowder.class.getSimpleName();
+        return RainbowNeutralizer.class.getSimpleName();
     }
 }
