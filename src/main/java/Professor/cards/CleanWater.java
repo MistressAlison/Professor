e805b44eb6e02a747edff1d609f202e47a95a1b4
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-09 21:11:46 -0300

    Rework Clear Water

--- src/main/java/Professor/cards/CleanWater.java
+++ src/main/java/Professor/cards/CleanWater.java
@@ -1,7 +1,6 @@
 package Professor.cards;
 
 import Professor.cards.abstracts.AbstractEasyCard;
-import Professor.patches.CustomTags;
 import Professor.util.CardArtRoller;
 import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -15,7 +14,8 @@ public class CleanWater extends AbstractEasyCard {
     public CleanWater() {
         super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
         baseBlock = block = 4;
-        tags.add(CustomTags.PROF_REACTANT);
+        //tags.add(CustomTags.PROF_REACTANT);
+        selfRetain = true;
     }
 
     @Override
