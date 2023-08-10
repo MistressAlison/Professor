c47e1f62559d4482d7fb3222c7898710ade26283
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-10 00:28:03 -0300

    Add Gold Uni

--- src/main/java/Professor/cards/GoldUni.java
+++ src/main/java/Professor/cards/GoldUni.java
@@ -1,13 +1,15 @@
-package Professor.cutStuff.cards;
+package Professor.cards;
 
-import CardAugments.util.Wiz;
+import Professor.actions.DamageFollowupAction;
 import Professor.cards.abstracts.AbstractEasyCard;
-import Professor.patches.ArchetypeHelper;
 import Professor.patches.CustomTags;
 import Professor.util.CardArtRoller;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -19,9 +21,9 @@ public class GoldUni extends AbstractEasyCard {
     public final static String ID = makeID(GoldUni.class.getSimpleName());
 
     public GoldUni() {
-        super(ID, 3, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = damage = 24;
-        baseMagicNumber = magicNumber = 4;
+        super(ID, 3, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 20;
+        baseMagicNumber = magicNumber = 2;
         tags.add(CustomTags.PROF_UNI);
     }
 
@@ -30,43 +32,24 @@ public class GoldUni extends AbstractEasyCard {
         if (m != null) {
             addToBot(new VFXAction(new VerticalImpactEffect(m.hb.cX + m.hb.width / 4.0F, m.hb.cY - m.hb.height / 4.0F)));
         }
-        dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
-    }
-
-    @Override
-    public void calculateCardDamage(AbstractMonster mo) {
-        int lastMagic = magicNumber;
-        int basebase = baseDamage;
-        baseDamage += magicNumber * countCards();
-        super.calculateCardDamage(mo);
-        baseDamage = basebase;
-        isDamageModified = baseDamage != damage;
-        if (lastMagic != magicNumber) {
-            super.calculateCardDamage(mo);
-        }
-    }
-
-    @Override
-    public void applyPowers() {
-        int lastMagic = magicNumber;
-        int basebase = baseDamage;
-        baseDamage += magicNumber * countCards();
-        super.applyPowers();
-        baseDamage = basebase;
-        isDamageModified = baseDamage != damage;
-        if (lastMagic != magicNumber) {
-            super.applyPowers();
-        }
-    }
-
-    private int countCards() {
-        return (int) Wiz.cardsPlayedThisCombat().stream().filter(ArchetypeHelper::isBolt).count();
+        addToBot(new DamageFollowupAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE, t -> {
+            if (t.isDying || t.currentHealth <= 0) {
+                addToTop(new DrawCardAction(magicNumber, new AbstractGameAction() {
+                    @Override
+                    public void update() {
+                        for (AbstractCard c : DrawCardAction.drawnCards) {
+                            c.setCostForTurn(0);
+                        }
+                        this.isDone = true;
+                    }
+                }));
+            }
+        }));
     }
 
     @Override
     public void upp() {
         upgradeDamage(8);
-        upgradeMagicNumber(1);
     }
 
     @Override
