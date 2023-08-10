b508c5742fbf99f3cca7473741f04d0b386fed0c
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-10 01:58:07 -0300

    Add Wind Shoes

---
+++ src/main/java/Professor/cards/WindShoes.java
@@ -0,0 +1,55 @@
+package Professor.cards;
+
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.util.CardArtRoller;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.WhirlwindEffect;
+
+import static Professor.MainModfile.makeID;
+
+public class WindShoes extends AbstractEasyCard {
+    public final static String ID = makeID(WindShoes.class.getSimpleName());
+
+    public WindShoes() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = block = 9;
+        baseMagicNumber = magicNumber = 2;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        addToBot(new SFXAction("ATTACK_WHIFF_2", 0.2f));
+        addToBot(new VFXAction(new WhirlwindEffect(new Color(1.0F, 0.9F, 0.4F, 1.0F), true)));
+        addToBot(new DrawCardAction(p, magicNumber));
+        addToBot(new DiscardAction(p, p, magicNumber, false));
+    }
+
+    @Override
+    public void upp() {
+        upgradeBlock(1);
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, Color.BROWN, WHITE, Color.BROWN, WHITE, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return VoidCard.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return WindShoes.class.getSimpleName();
+    }
+}
