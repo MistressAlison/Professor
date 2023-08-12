04b72d286b43a64f3efe8e3c9632ce36a00bc850
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-11 23:12:54 -0300

    Register more powers

--- src/main/java/Professor/MainModfile.java
+++ src/main/java/Professor/MainModfile.java
@@ -3,7 +3,10 @@ package Professor;
 import Professor.cards.cardvars.*;
 import Professor.icons.IconContainer;
 import Professor.patches.ArchetypeHelper;
+import Professor.powers.BracedPower;
+import Professor.powers.ExposedPower;
 import Professor.powers.FocusPower;
+import Professor.powers.StaggerPower;
 import Professor.relics.AbstractEasyRelic;
 import Professor.ui.SynthesisPanel;
 import Professor.util.CustomSounds;
@@ -262,6 +265,9 @@ public class MainModfile implements
 
         //Add Powers
         BaseMod.addPower(FocusPower.class, FocusPower.POWER_ID);
+        BaseMod.addPower(ExposedPower.class, ExposedPower.POWER_ID);
+        BaseMod.addPower(BracedPower.class, BracedPower.POWER_ID);
+        BaseMod.addPower(StaggerPower.class, StaggerPower.POWER_ID);
 
         //Wide Potions
 
