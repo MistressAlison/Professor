8ff6c309256845f2f7427118ec4fff869143520c
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-11 22:16:56 -0300

    Rework Lunar Drop

--- src/main/java/Professor/cards/LunarDrop.java
+++ src/main/java/Professor/cards/LunarDrop.java
@@ -1,46 +1,34 @@
 package Professor.cards;
 
-import CardAugments.util.Wiz;
 import Professor.cards.abstracts.AbstractEasyCard;
-import Professor.patches.ArchetypeHelper;
+import Professor.cards.interfaces.InstantSynthesisCard;
+import Professor.cards.interfaces.OnUseInSynthesisCard;
+import Professor.ui.SynthesisItem;
 import Professor.util.CardArtRoller;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Professor.MainModfile.makeID;
 
-public class LunarDrop extends AbstractEasyCard {
+public class LunarDrop extends AbstractEasyCard implements OnUseInSynthesisCard, InstantSynthesisCard {
     public final static String ID = makeID(LunarDrop.class.getSimpleName());
 
     public LunarDrop() {
-        super(ID, 2, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
-        baseBlock = block = 5;
-        baseInfo = info = 0;
-        exhaust = true;
+        super(ID, -2, CardType.SKILL, CardRarity.RARE, CardTarget.NONE);
+        selfRetain = true;
     }
 
-    @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new GainBlockAction(p, block * (countCards()-1)));
+    public void use(AbstractPlayer p, AbstractMonster m) {}
+
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
     }
 
     @Override
     public void upp() {
-        upgradeBlock(2);
-    }
-
-    @Override
-    protected void applyPowersToBlock() {
-        super.applyPowersToBlock();
-        baseInfo = baseBlock * countCards();
-        info = block * countCards();
-        isInfoModified = baseInfo != info;
-    }
-
-    private int countCards() {
-        return (int) Wiz.cardsPlayedThisCombat().stream().filter(ArchetypeHelper::isIce).count();
+        uDesc();
     }
 
     @Override
@@ -56,5 +44,19 @@ public class LunarDrop extends AbstractEasyCard {
     @Override
     public String itemArt() {
         return LunarDrop.class.getSimpleName();
+    }
+
+    @Override
+    public boolean onAdded(SynthesisItem item) {
+        superFlash();
+        if (upgraded && item.currentCreation.canUpgrade()) {
+            item.currentCreation.upgrade();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isInstant(SynthesisItem item) {
+        return true;
     }
 }
