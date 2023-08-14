e74b37ddc982fd064059e665ee72965659861540
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-14 18:32:31 -0300

    Rework Clean Water

--- src/main/java/Professor/cards/CleanWater.java
+++ src/main/java/Professor/cards/CleanWater.java
@@ -1,28 +1,27 @@
 package Professor.cards;
 
 import Professor.cards.abstracts.AbstractEasyCard;
-import Professor.cards.interfaces.OnUseInSynthesisCard;
-import Professor.ui.SynthesisItem;
 import Professor.util.CardArtRoller;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Professor.MainModfile.makeID;
 
-public class CleanWater extends AbstractEasyCard implements OnUseInSynthesisCard {
+public class CleanWater extends AbstractEasyCard {
     public final static String ID = makeID(CleanWater.class.getSimpleName());
 
     public CleanWater() {
         super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
-        baseBlock = block = 4;
-        baseMagicNumber = magicNumber = 3;
+        baseBlock = block = 8;
         //tags.add(CustomTags.PROF_REACTANT);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
+        addToBot(new DiscardAction(p, p, 1, false));
     }
 
     @Override
@@ -45,10 +44,4 @@ public class CleanWater extends AbstractEasyCard implements OnUseInSynthesisCard
         return CleanWater.class.getSimpleName();
     }
 
-    @Override
-    public boolean onAdded(SynthesisItem item) {
-        superFlash();
-        baseBlock += magicNumber;
-        return false;
-    }
 }
