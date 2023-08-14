e7fa5aa5254ca140533e475afa0fd24d6b3d4c35
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-14 20:12:26 -0300

    Meltstone buffed, moved to Uncommon

--- src/main/java/Professor/cards/Meltstone.java
+++ src/main/java/Professor/cards/Meltstone.java
@@ -1,7 +1,9 @@
 package Professor.cards;
 
+import Professor.cardmods.UnlockedMod;
 import Professor.cards.abstracts.AbstractEasyCard;
 import Professor.util.CardArtRoller;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustAction;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
@@ -14,21 +16,21 @@ public class Meltstone extends AbstractEasyCard {
     public final static String ID = makeID(Meltstone.class.getSimpleName());
 
     public Meltstone() {
-        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         baseDamage = damage = 9;
         baseMagicNumber = magicNumber = 1;
+        CardModifierManager.addModifier(this, new UnlockedMod());
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.FIRE);
-        addToBot(new ExhaustAction(magicNumber, !upgraded, false, false));
+        addToBot(new ExhaustAction(magicNumber, false, false, false));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(2);
-        uDesc();
+        upgradeDamage(2+timesUpgraded);
     }
 
     @Override
