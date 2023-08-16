938ccf8b380c78a060fb0b91f54362d5a10c4c71
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-16 20:17:46 -0300

    Add Blue Star Robe

---
+++ src/main/java/Professor/cards/BlueStarRobe.java
@@ -0,0 +1,49 @@
+package Professor.cards;
+
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.util.CardArtRoller;
+import Professor.util.Wiz;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+
+import static Professor.MainModfile.makeID;
+
+public class BlueStarRobe extends AbstractEasyCard {
+    public final static String ID = makeID(BlueStarRobe.class.getSimpleName());
+
+    public BlueStarRobe() {
+        super(ID, 2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseBlock = block = 13;
+        baseMagicNumber = magicNumber = 2;
+        //tags.add(CustomTags.PROF_REACTANT);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        Wiz.applyToSelf(new DrawCardNextTurnPower(p, magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        upgradeBlock(4);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, mix(Color.GRAY, AZURE), WHITE, mix(Color.GRAY, AZURE), BLACK, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return BlueStarRobe.class.getSimpleName();
+    }
+}
