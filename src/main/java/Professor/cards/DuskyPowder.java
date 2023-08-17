e806d9946be9f166ef2630772ea73db041b05d63
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-16 22:03:53 -0300

    Add Dusk Powder

---
+++ src/main/java/Professor/cards/DuskyPowder.java
@@ -0,0 +1,47 @@
+package Professor.cards;
+
+import Professor.actions.InfuseCardsInHandAction;
+import Professor.cardmods.ApplyDestabilizedMod;
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.util.CardArtRoller;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Professor.MainModfile.makeID;
+
+public class DuskyPowder extends AbstractEasyCard {
+    public final static String ID = makeID(DuskyPowder.class.getSimpleName());
+
+    public DuskyPowder() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+        baseMagicNumber = magicNumber = 2;
+        exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new InfuseCardsInHandAction(p.hand.size(), new ApplyDestabilizedMod(magicNumber)));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, mix(Color.GRAY, Color.PURPLE), WHITE, mix(Color.GRAY, Color.PURPLE), BLACK, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return DuskyPowder.class.getSimpleName();
+    }
+}
