1d297db67a7c41cabc505428c975fb202248becb
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-17 01:16:52 -0300

    Add Black Water

---
+++ src/main/java/Professor/cards/BlackWater.java
@@ -0,0 +1,49 @@
+package Professor.cards;
+
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.powers.BlackWaterPower;
+import Professor.util.CardArtRoller;
+import Professor.util.Wiz;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Professor.MainModfile.makeID;
+
+public class BlackWater extends AbstractEasyCard {
+    public final static String ID = makeID(BlackWater.class.getSimpleName());
+
+    public BlackWater() {
+        super(ID, 1, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = 2;
+        //tags.add(CustomTags.PROF_REACTANT);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.applyToSelf(new BlackWaterPower(p, magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+        //upgradeBaseCost(0);
+        //isInnate = true;
+        //uDesc();
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, mix(WHITE, BLACK), WHITE, mix(WHITE, BLACK), WHITE, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return VoidCard.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return BlackWater.class.getSimpleName();
+    }
+}
---
+++ src/main/java/Professor/powers/BlackWaterPower.java
@@ -0,0 +1,67 @@
+package Professor.powers;
+
+import Professor.MainModfile;
+import Professor.cards.BlackWater;
+import Professor.util.PowerIconMaker;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.BetterOnApplyPowerPower;
+import com.evacipated.cardcrawl.modthespire.lib.SpireField;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class BlackWaterPower extends AbstractPower implements BetterOnApplyPowerPower {
+    public static final String POWER_ID = MainModfile.makeID(BlackWaterPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public BlackWaterPower(AbstractCreature owner, int amount) {
+        this.ID = POWER_ID;
+        this.name = NAME;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+        PowerIconMaker.setIcons(this, BlackWater.class.getSimpleName());
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
+        if (damageAmount > 0 && target != this.owner && info.type == DamageInfo.DamageType.NORMAL) {
+            flash();
+            addToTop(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(this.amount, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.FIRE, true));
+        }
+    }
+
+    @Override
+    public boolean betterOnApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
+        if (source == owner && target != owner && power.type == PowerType.DEBUFF && !LoopField.looping.get(power)) {
+            flash();
+            AbstractPower p =  new DestabilizedPower(target, owner, amount);
+            LoopField.looping.set(p, true);
+            addToBot(new ApplyPowerAction(target, owner, p));
+        }
+        return true;
+    }
+
+    @Override
+    public int betterOnApplyPowerStacks(AbstractPower power, AbstractCreature target, AbstractCreature source, int stackAmount) {
+        return BetterOnApplyPowerPower.super.betterOnApplyPowerStacks(power, target, source, stackAmount);
+    }
+
+    @SpirePatch2(clz = AbstractPower.class, method = SpirePatch.CLASS)
+    public static class LoopField {
+        public static SpireField<Boolean> looping = new SpireField<>(() -> false);
+    }
+}
