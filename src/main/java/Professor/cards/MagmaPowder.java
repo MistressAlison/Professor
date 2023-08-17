4ace672421711ef1e1da68d0213fcd7429a9b038
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-16 22:03:43 -0300

    Add Magma Powder

---
+++ src/main/java/Professor/cards/MagmaPowder.java
@@ -0,0 +1,68 @@
+package Professor.cards;
+
+import Professor.cards.abstracts.AbstractEasyCard;
+import Professor.patches.OnUseEnergyPatches;
+import Professor.util.CardArtRoller;
+import Professor.util.Wiz;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Professor.MainModfile.makeID;
+
+public class MagmaPowder extends AbstractEasyCard implements OnUseEnergyPatches.OnUseEnergyObject {
+    public final static String ID = makeID(MagmaPowder.class.getSimpleName());
+
+    public MagmaPowder() {
+        super(ID, -2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
+        baseMagicNumber = magicNumber = 2;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {}
+
+    @Override
+    public void triggerOnOtherCardPlayed(AbstractCard c) {
+        addToBot(new DamageAllEnemiesAction(Wiz.adp(), DamageInfo.createDamageMatrix(magicNumber, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.FIRE, true));
+    }
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public void onUseEnergy(int amount) {
+        if (amount > 0 && Wiz.adp().hand.contains(this)) {
+            flash();
+            addToBot(new DrawCardAction(magicNumber));
+        }
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, mix(Color.GRAY, Color.RED), WHITE, mix(Color.GRAY, Color.RED), BLACK, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Miracle.ID;
+    }
+
+    @Override
+    public String itemArt() {
+        return MagmaPowder.class.getSimpleName();
+    }
+}
