edb8ed35f5545c5068f94d0c8e6a1b98e901b5d8
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-21 21:19:16 -0300

    Fix Lightning Essence upgrade

--- src/main/java/Professor/cards/LightningEssence.java
+++ src/main/java/Professor/cards/LightningEssence.java
@@ -26,7 +26,7 @@ public class LightningEssence extends AbstractEasyCard {
     @Override
     public void upp() {
         //upgradeMagicNumber(1);
-        upgradeBaseCost(0);
+        upgradeBaseCost(1);
     }
 
     @Override
