71482233ebdd020565650ae88c3254e87e50130a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-21 23:05:21 -0300

    Rework Wind Shoes, move to Common

--- src/main/java/Professor/cards/WindShoes.java
+++ src/main/java/Professor/cards/WindShoes.java
@@ -2,6 +2,7 @@ package Professor.cards;
 
 import Professor.cards.abstracts.AbstractEasyCard;
 import Professor.util.CardArtRoller;
+import Professor.util.Wiz;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
@@ -9,6 +10,7 @@ import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
 import com.megacrit.cardcrawl.vfx.combat.WhirlwindEffect;
 
 import static Professor.MainModfile.makeID;
@@ -17,9 +19,9 @@ public class WindShoes extends AbstractEasyCard {
     public final static String ID = makeID(WindShoes.class.getSimpleName());
 
     public WindShoes() {
-        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
         baseBlock = block = 6;
-        baseMagicNumber = magicNumber = 2;
+        baseMagicNumber = magicNumber = 1;
     }
 
     @Override
@@ -28,13 +30,14 @@ public class WindShoes extends AbstractEasyCard {
         addToBot(new SFXAction("ATTACK_WHIFF_2", 0.2f));
         addToBot(new VFXAction(new WhirlwindEffect(new Color(1.0F, 0.9F, 0.4F, 1.0F), true)));
         addToBot(new DrawCardAction(p, magicNumber));
+        Wiz.applyToSelf(new DrawCardNextTurnPower(p, magicNumber));
         //addToBot(new DiscardAction(p, p, magicNumber, false));
     }
 
     @Override
     public void upp() {
-        //upgradeBlock(3);
-        upgradeMagicNumber(1);
+        upgradeBlock(3);
+        //upgradeMagicNumber(1);
     }
 
     @Override
