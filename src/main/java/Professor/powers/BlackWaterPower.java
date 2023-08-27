dc26baaf60b16b879f92842e6724a0fb5ad223cc
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-26 23:29:08 -0300

    Black Water only triggers if the power is not negated

--- src/main/java/Professor/powers/BlackWaterPower.java
+++ src/main/java/Professor/powers/BlackWaterPower.java
@@ -1,20 +1,11 @@
 package Professor.powers;
 
 import Professor.MainModfile;
+import Professor.actions.DoIfPowerAppliedAction;
 import Professor.cards.BlackWater;
-import Professor.damageMods.BlackWaterDamage;
 import Professor.util.PowerIconMaker;
-import com.evacipated.cardcrawl.mod.stslib.damagemods.AbstractDamageModifier;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.BetterOnApplyPowerPower;
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.DamageModApplyingPower;
-import com.evacipated.cardcrawl.modthespire.lib.SpireField;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
@@ -22,15 +13,11 @@ import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
 
-import java.util.Collections;
-import java.util.List;
-
 public class BlackWaterPower extends AbstractPower implements BetterOnApplyPowerPower{
     public static final String POWER_ID = MainModfile.makeID(BlackWaterPower.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-    //private final BlackWaterDamage bwd = new BlackWaterDamage(0);
 
     public BlackWaterPower(AbstractCreature owner, int amount) {
         this.ID = POWER_ID;
@@ -47,23 +34,11 @@ public class BlackWaterPower extends AbstractPower implements BetterOnApplyPower
         this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
     }
 
-    /*@Override
-    public boolean shouldPushMods(DamageInfo damageInfo, Object o, List<AbstractDamageModifier> list) {
-        return o instanceof AbstractCard && ((AbstractCard) o).type == AbstractCard.CardType.ATTACK;
-    }
-
-    @Override
-    public List<AbstractDamageModifier> modsToPush(DamageInfo damageInfo, Object o, List<AbstractDamageModifier> list) {
-        bwd.setAmount(25*amount);
-        return Collections.singletonList(bwd);
-    }*/
-
-
     @Override
     public boolean betterOnApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
         if (source == owner && target != owner && (power instanceof WeakPower || power instanceof VulnerablePower)) {
             flash();
-            addToBot(new ApplyPowerAction(target, owner, new UnstablePower(target, amount)));
+            addToBot(new DoIfPowerAppliedAction(power, new ApplyPowerAction(target, owner, new UnstablePower(target, amount))));
         }
         return true;
     }
