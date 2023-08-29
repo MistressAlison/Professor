24f4d91ca4fb65a48adc0e5872d7cd7bacea699f
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-29 19:53:05 -0300

    Fix DrawManipPatches

--- src/main/java/Professor/patches/DrawManipPatches.java
+++ src/main/java/Professor/patches/DrawManipPatches.java
@@ -2,21 +2,21 @@ package Professor.patches;
 
 import Professor.powers.interfaces.DrawManipPower;
 import Professor.util.Wiz;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
 public class DrawManipPatches {
-    @SpirePatch2(clz = DrawCardAction.class, method = SpirePatch.CONSTRUCTOR, paramtypez = {AbstractCreature.class, int.class, boolean.class})
+    @SpirePatch2(clz = DrawCardAction.class, method = "update")
     public static class ModifyAmount {
-        @SpirePostfixPatch
-        public static void modify(DrawCardAction __instance) {
-            for (AbstractPower p : Wiz.adp().powers) {
-                if (p instanceof DrawManipPower) {
-                    __instance.amount = ((DrawManipPower) p).changeAmount(__instance.amount);
+        @SpirePrefixPatch
+        public static void modify(DrawCardAction __instance, boolean ___clearDrawHistory) {
+            if (___clearDrawHistory) {
+                for (AbstractPower p : Wiz.adp().powers) {
+                    if (p instanceof DrawManipPower) {
+                        __instance.amount = ((DrawManipPower) p).changeAmount(__instance.amount);
+                    }
                 }
             }
         }
