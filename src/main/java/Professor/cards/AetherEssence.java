f4e0ba2b66c4f67fd511db5de4dbf211fdf10a61
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-08-29 14:00:51 -0300

    Aether Essence now pink

--- src/main/java/Professor/cards/AetherEssence.java
+++ src/main/java/Professor/cards/AetherEssence.java
@@ -35,7 +35,7 @@ public class AetherEssence extends AbstractEasyCard {
 
     @Override
     public CardArtRoller.ReskinInfo reskinInfo(String ID) {
-        return new CardArtRoller.ReskinInfo(ID, ORANGE, BLACK, Color.GRAY, DEEP_PINK, false);
+        return new CardArtRoller.ReskinInfo(ID, ORANGE, BLACK, pastel(DEEP_PINK), pastel(PERSIMMON), false);
     }
 
     @Override
